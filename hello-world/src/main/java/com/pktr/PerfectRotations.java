8787500a93c8c89f56f4c817b95760646c7dbd65
Pradeep Kumar <pkumar@homeaway.com>
2013-10-18 18:55:47 +0530

    Perfect rotations

---
+++ hello-world/src/main/java/com/pktr/PerfectRotations.java
@@ -0,0 +1,42 @@
+package com.pktr;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class PerfectRotations {
+
+    public static boolean areRotations(String s, String t) {
+        Map<Character, Character> originNextCMap = getNextCharMap(s);
+        Map<Character, Character> destNextCMap = getNextCharMap(t);
+        int count = 0;
+        for (Entry<Character, Character> entry : originNextCMap.entrySet()) {
+            Character key = entry.getKey();
+            if (entry.getValue().equals(destNextCMap.get(key))) {
+                count++;
+            }
+        }
+        return (count == originNextCMap.size() && count == destNextCMap.size());
+
+    }
+
+    private static Map<Character, Character> getNextCharMap(String s) {
+        Map<Character, Character> nextCharMap = new HashMap<Character, Character>();
+        char[] sArray = s.toCharArray();
+        for (int i = 0; i < sArray.length; i++) {
+            int nextIndex = i + 1;
+            if (nextIndex == sArray.length) {
+                nextIndex = 0;
+            }
+            nextCharMap.put(sArray[i], sArray[nextIndex]);
+        }
+        return nextCharMap;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PerfectRotations.areRotations("tackoverflows", "stackoverflow"));
+        System.out.println(PerfectRotations.areRotations("hello", "elloh"));
+    }
+
+}
